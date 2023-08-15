e79abd707ecd0658c71b84ad11823fc3da15fad2
Maximus <Maxime_NewErA@outlook.com>
2023-08-15 14:16:43 +0800

    ✨ feat: Add Data Preparation Script

---
+++ run_prepare_data.sh
@@ -0,0 +1,12 @@
+cd examples/language_model/
+bash prepare-wikitext-103.sh
+cd ../..
+
+TEXT=examples/language_model/wikitext-103
+python preprocess.py \
+    --only-source \
+    --trainpref $TEXT/wiki.train.tokens \
+    --validpref $TEXT/wiki.valid.tokens \
+    --testpref $TEXT/wiki.test.tokens \
+    --destdir data-bin/wikitext-103 \
+    --workers 20
